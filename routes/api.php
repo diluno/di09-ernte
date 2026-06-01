56acb7a758d41e60ff9306934653132ab1cf6e87
Samuel Alder <samuel.alder@gmail.com>
2026-06-01 13:57:04 +0200

    chore(api): import MeController for consistent route style

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\MeController;
 use App\Http\Controllers\Api\TimerController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +9,7 @@ Route::post('/auth/token', [AuthController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/auth/token', [AuthController::class, 'destroy']);
-    Route::get('/me', [\App\Http\Controllers\Api\MeController::class, 'show']);
+    Route::get('/me', [MeController::class, 'show']);
     Route::get('/timer', [TimerController::class, 'show']);
     Route::post('/timer/start', [TimerController::class, 'start']);
     Route::post('/timer/switch', [TimerController::class, 'switch']);
